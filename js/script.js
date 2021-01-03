4aa87d71754d9f71cde7a5222226a9dbd3cc7360
dp.ua <dp.ua@mail.com>
2021-01-03 20:53:01 +0200

    writeYourGenres

--- js/script.js
+++ js/script.js
@@ -29,12 +29,44 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for(let i=0; i< 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''); 
+
+      if(a != null && b != null && a != '' && b != '' && a.length<50){
+        personalMovieDB.movies[a] = b;
+      }else{
+          i--;
+      }
+
+}
+
+if (personalMovieDB.count<10){
+
+}else if(personalMovieDB.count>=10 && personalMovieDB.count<30) {
+    
+}else if(personalMovieDB.count>=30) {
+    
+}else{
+    
+}
+
+function writeYourGenres(){
+    for(let i=1;i<=3;i++){
+        personalMovieDB.genres[i-1]=prompt(`Your ganre ${i}`)
+    }
+}
+writeYourGenres();
 
 console.log(personalMovieDB);
+
+
+for(let i=1;i<10;i++){
+    if(i===6){
+        //break;
+        continue;
+    }
+    console.log(i);
+}
